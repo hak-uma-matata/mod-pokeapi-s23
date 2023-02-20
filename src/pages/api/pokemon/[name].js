78fbb7c42b3de6f6a94003ae7193620e1964a2df
Uma <uma2005anand@gmail.com>
2023-02-19 20:33:31 -0500

    feat: finished name without search

--- src/pages/api/pokemon/[name].js
+++ src/pages/api/pokemon/[name].js
@@ -1,6 +1,15 @@
 // https://pokeapi.co/api/v2/pokemon/{id or name}/
 import axios from "axios";
 export default async function handler(req, res) {
-    console.log(req.query);
-    res.send("2");
+    let response;
+    try {
+        response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + req.query.name);        
+    } catch (e) {
+        console.log(e);
+    }
+    let data = response.data;
+    data.types = data.types.map((type) => {
+        return type.type.name;
+    });
+    return res.send({name: data.name, sprite: data.sprites.front_default, types: data.types});
 }
