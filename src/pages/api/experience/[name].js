a3c1daf5c72ade3552b7f93b76eb4212927584ba
Uma <uma2005anand@gmail.com>
2023-02-21 19:14:58 -0500

    feat: finished exp

--- src/pages/api/experience/[name].js
+++ src/pages/api/experience/[name].js
@@ -1,17 +1,26 @@
-/** 
+
 import axios from "axios";
 export default async function handler(req, res) {
     let response;
     try {
-        response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + req.query.name);        
+        response = await axios.get("https://pokeapi.co/api/v2/pokemon-species/" + req.query.name);        
     } catch (e) {
         console.log(e);
     }
+    try {
+        response = await axios.get(response.data.growth_rate.url);
+    } catch (e) {
+        console.log(e);
+    }
+    let level = req.query.level;
+    let exp = response.data.levels[level - 1].experience;
+    
+    /**
     let data = response.data;
+    
     data.types = data.types.map((type) => {
         return type.type.name;
     });
-    console.log(data.name);
-    return res.send({pokemonName: data.name, sprite: data.sprites.front_default, types: data.types});
+    console.log(data.name); */
+    return res.send({experience: exp});
 }
-*/
